8e395a428e0f44e2480f47b3e2914bd90970fecd
Asguho <78410939+Asguho@users.noreply.github.com>
2022-10-01 14:30:45 +0200

    Update versions.pl

--- versions.pl
+++ versions.pl
@@ -4,7 +4,9 @@ use strict;
 open FILE "pack.toml" || die "cant find pack.toml";
 my $file=join('',<FILE>);
 close FILE;
-my $mcversion
+
+my $mcversion;
+
 if ($file=~/minecraft = "([^"]+)"/) {
     $mcversion = $1; 
 }
