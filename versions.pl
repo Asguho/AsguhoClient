efd04203b255bdd80d8d9bac60a97cea6ef14237
Asguho <78410939+Asguho@users.noreply.github.com>
2022-10-01 14:16:47 +0200

    Update versions.pl

--- versions.pl
+++ versions.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
 use strict;
 
-print "::set-output name=zz::1.6.5";
-print "::set-output name=bb::1.6.9";
+print "::set-output name=zz::1.6.5;";
+print "::set-output name=bb::1.6.9;";
