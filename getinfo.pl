05cefa772b7193114e01a4e3cc9f52852b211815
Asguho <78410939+Asguho@users.noreply.github.com>
2022-10-02 10:07:31 +0200

    Update getinfo.pl

--- getinfo.pl
+++ getinfo.pl
@@ -11,18 +11,31 @@ sub getMCVersion(){
     }
 }
 sub getModPackVersion(){
-
+    my previousversion = ARGV[1];
 }
 
 
 sub isItNew(){
-    
+    open FILE, '<', ".minecraft/AsguhoClient-1.0.0.mrpack" || die $!;
+        my $file1=join('',<FILE>);
+    close FILE;
+
+    open FILE, '<', "AsguhoClient-1.0.0.mrpack" || die $!;
+        my $file2=join('',<FILE>);
+    close FILE;
+    return !compare_text($file1, $file2)
 }
 
-print "::set-output name=isitnew::"+isItNew();
+if($ARGV[0] == 'isitnew'){
+    print "::set-output name=$ARGV[0]::"+isItNew();
+}
 
-print "::set-output name=modpackversion::"+getModPackVersion();
+if($ARGV[0] == 'modpackversion'){
+    print "::set-output name=$ARGV[0]::"+getModPackVersion();
+}
 
-print "::set-output name=mcversion::"+getMCVersion();
+if($ARGV[0] == 'mcversion'){
+    print "::set-output name=$ARGV[0]::"+getMCVersion();
+}
 
 
