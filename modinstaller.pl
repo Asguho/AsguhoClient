d7cb5d10e8d1e83a9ebfe12ba09acff667529920
Asguho <78410939+Asguho@users.noreply.github.com>
2022-10-02 17:25:18 +0200

    Update modinstaller.pl

--- modinstaller.pl
+++ modinstaller.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 use strict;
-my @modrinthMods = ('fabric-api','lithium', 'c2me-fabric', 'starlight', 'lazydfu', 'debugify', 'smoothboot-fabric', 'sodium', 'ebe', 'memoryleakfix','krypton','vmp-fabric','ferrite-core','exordium','fastload','ImmediatelyFast', 'modmenu', 'mouse-wheelie');
-my @curseforgeMods = ('litematica', 'minihud', 'tellme', 'tweakeroo', 'worldedit', 'carpet');
+my @modrinthMods = ('lithium', 'c2me-fabric', 'starlight', 'lazydfu', 'debugify', 'smoothboot-fabric', 'sodium', 'ebe', 'memoryleakfix','krypton','vmp-fabric','ferrite-core','exordium','fastload','ImmediatelyFast', 'modmenu', 'mouse-wheelie');
+my @curseforgeMods = ('fabric-api','litematica', 'minihud', 'tellme', 'tweakeroo', 'worldedit', 'carpet');
 
 for(@modrinthMods){
 	system("packwiz mr install $_ -y");
