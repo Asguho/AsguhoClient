45ffd98176056a4d268f93d353a6e9b8b77fac03
Asguho <78410939+Asguho@users.noreply.github.com>
2022-10-06 14:13:13 +0200

    updated version

--- modinstaller.pl
+++ modinstaller.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 use strict;
-my @modrinthMods = ('lithium', 'c2me-fabric', 'starlight', 'lazydfu', 'debugify', 'smoothboot-fabric', 'sodium', 'ebe', 'memoryleakfix','krypton','vmp-fabric','ferrite-core','exordium','fastload','ImmediatelyFast', 'modmenu', 'mouse-wheelie','no-chat-reports');
+my @modrinthMods = ('lithium', 'c2me-fabric', 'starlight', 'lazydfu', 'debugify', 'smoothboot-fabric', 'sodium', 'ebe', 'memoryleakfix','krypton','vmp-fabric','ferrite-core','exordium','fastload','ImmediatelyFast', 'modmenu', 'mouse-wheelie','no-chat-reports','multiconnect');
 my @curseforgeMods = ('fabric-api','litematica', 'minihud', 'tellme', 'tweakeroo','item-scroller', 'worldedit', 'carpet','xaeros-world-map','xaeros-minimap-fair-play-edition');
 
 for(@modrinthMods){
