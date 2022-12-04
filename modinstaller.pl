13afdd4c07e05e16807f0ee07dc1a3dd5718b5e0
Asguho <78410939+Asguho@users.noreply.github.com>
2022-12-04 19:49:20 +0100

    moved mods to be installed with forge

--- modinstaller.pl
+++ modinstaller.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 use strict;
-my @modrinthMods = ('sodium','exordium','memoryleakfix');
+my @modrinthMods = ('exordium');
 my @curseforgeMods = ('litematica', 'minihud', 'tellme', 'tweakeroo','item-scroller', 'worldedit', 'carpet','xaeros-world-map','xaeros-minimap-fair-play-edition');
-my @curseforgeModsNeededReason = ('fabric-api','c2me-fabric','debugify','enhanced-block-entities','fastload','ferritecore-fabric','ImmediatelyFast','krypton','lazydfu','lithium','starlight','smoothboot-fabric','vmp-fabric', 'modmenu', 'mouse-wheelie','no-chat-reports','multiconnect','yosbr');
+my @curseforgeModsNeededReason = ('fabric-api','c2me-fabric','debugify','enhanced-block-entities','fastload','ferritecore-fabric','ImmediatelyFast','krypton','lazydfu','lithium','starlight','smoothboot-fabric','vmp-fabric', 'modmenu', 'mouse-wheelie','no-chat-reports','multiconnect','yosbr', 'sodium','memoryleakfix');
 
 for(@modrinthMods){
 	system("packwiz mr install $_ -y");
