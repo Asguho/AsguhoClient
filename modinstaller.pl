14c6684edf4d627fc0b685578b49e32e50b6b109
Asguho <78410939+Asguho@users.noreply.github.com>
2022-12-04 20:11:02 +0100

    Update modinstaller.pl

--- modinstaller.pl
+++ modinstaller.pl
@@ -2,7 +2,8 @@
 use strict;
 my @modrinthMods = ('exordium');
 my @curseforgeMods = ('litematica', 'minihud', 'tellme', 'tweakeroo','item-scroller', 'worldedit', 'carpet','xaeros-world-map','xaeros-minimap-fair-play-edition');
-my @curseforgeModsNeededReason = ('fabric-api','c2me-fabric','debugify','enhanced-block-entities','fastload','ferritecore-fabric','ImmediatelyFast','krypton','lazydfu','lithium','starlight','smoothboot-fabric','vmp-fabric', 'modmenu', 'mouse-wheelie','no-chat-reports','multiconnect','yosbr', 'sodium','memoryleakfix');
+my @curseforgeModsNeededReason = ('fabric-api','c2me-fabric','debugify','enhanced-block-entities','fastload','ferritecore-fabric','ImmediatelyFast','krypton','lazydfu','lithium','starlight','smoothboot-fabric','vmp-fabric', 'modmenu', 'mouse-wheelie','no-chat-reports','multiconnect','yosbr', 'sodium');
+#,'memoryleakfix'
 
 for(@modrinthMods){
 	system("packwiz mr install $_ -y");
