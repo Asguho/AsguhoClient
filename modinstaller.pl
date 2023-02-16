7326be4d53b9bf16715de9a70d1c4ca2ea235290
Asguho <78410939+Asguho@users.noreply.github.com>
2023-02-16 17:12:11 +0100

    removed multiconnect

--- modinstaller.pl
+++ modinstaller.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 use strict;
-my @modrinthMods = ('exordium', 'fabric-api','c2me-fabric','debugify','ebe','fastload','ferritecore-fabric','carpet','ImmediatelyFast','krypton','lazydfu','lithium','starlight','smoothboot-fabric','vmp-fabric', 'modmenu', 'mouse-wheelie','memoryleakfix','no-chat-reports','multiconnect', 'sodium','mixintrace', 'mixin-conflict-helper', 'notenoughcrashes');
+my @modrinthMods = ('exordium', 'fabric-api','c2me-fabric','debugify','ebe','fastload','ferritecore-fabric','carpet','ImmediatelyFast','krypton','lazydfu','lithium','starlight','smoothboot-fabric','vmp-fabric', 'modmenu', 'mouse-wheelie','memoryleakfix','no-chat-reports', 'sodium','mixintrace', 'mixin-conflict-helper', 'notenoughcrashes');
 my @curseforgeMods = ('litematica', 'minihud', 'tellme', 'tweakeroo','item-scroller', 'worldedit','xaeros-world-map','xaeros-minimap-fair-play-edition', 'yosbr');
-
+#multiconnect
 for(@modrinthMods){
 	system("packwiz mr install $_ -y");
 }
