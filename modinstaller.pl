11c429e0b220ea541d2875c614baa2cd5c257aef
Asguho <78410939+Asguho@users.noreply.github.com>
2023-02-16 12:55:53 +0100

    added error handling mods

--- modinstaller.pl
+++ modinstaller.pl
@@ -1,19 +1,13 @@
 #!/usr/bin/perl
 use strict;
-my @modrinthMods = ('exordium');
-my @curseforgeMods = ('litematica', 'minihud', 'tellme', 'tweakeroo','item-scroller', 'worldedit', 'carpet','xaeros-world-map','xaeros-minimap-fair-play-edition');
-my @curseforgeModsNeededReason = ('fabric-api','c2me-fabric','debugify','enhanced-block-entities','fastload','ferritecore-fabric','ImmediatelyFast','krypton','lazydfu','lithium','starlight','smoothboot-fabric','vmp-fabric', 'modmenu', 'mouse-wheelie','no-chat-reports','multiconnect','yosbr', 'sodium');
-#,'memoryleakfix'
+my @modrinthMods = ('exordium', 'fabric-api','c2me-fabric','debugify','enhanced-block-entities','fastload','ferritecore-fabric','carpet','ImmediatelyFast','krypton','lazydfu','lithium','starlight','smoothboot-fabric','vmp-fabric', 'modmenu', 'mouse-wheelie','memoryleakfix','no-chat-reports','multiconnect','yosbr', 'sodium','mixintrace', 'mixin-conflict-helper', 'notenoughcrashes');
+my @curseforgeMods = ('litematica', 'minihud', 'tellme', 'tweakeroo','item-scroller', 'worldedit','xaeros-world-map','xaeros-minimap-fair-play-edition');
 
 for(@modrinthMods){
 	system("packwiz mr install $_ -y");
 }
 
 for(@curseforgeMods){
-	system("packwiz cf install $_ -y");
-}
-
-for(@curseforgeModsNeededReason){
 	system("packwiz cf install $_ -y");
 }
 
