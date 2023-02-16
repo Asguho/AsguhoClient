cd6b5341358ca213944d342c0c6e046d4fe55340
Asguho <78410939+Asguho@users.noreply.github.com>
2023-02-16 16:40:27 +0100

    Update modinstaller.pl

--- modinstaller.pl
+++ modinstaller.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 use strict;
-my @modrinthMods = ('exordium', 'fabric-api','c2me-fabric','debugify','enhanced-block-entities','fastload','ferritecore-fabric','carpet','ImmediatelyFast','krypton','lazydfu','lithium','starlight','smoothboot-fabric','vmp-fabric', 'modmenu', 'mouse-wheelie','memoryleakfix','no-chat-reports','multiconnect','yosbr', 'sodium','mixintrace', 'mixin-conflict-helper', 'notenoughcrashes');
-my @curseforgeMods = ('litematica', 'minihud', 'tellme', 'tweakeroo','item-scroller', 'worldedit','xaeros-world-map','xaeros-minimap-fair-play-edition');
+my @modrinthMods = ('exordium', 'fabric-api','c2me-fabric','debugify','ebe','fastload','ferritecore-fabric','carpet','ImmediatelyFast','krypton','lazydfu','lithium','starlight','smoothboot-fabric','vmp-fabric', 'modmenu', 'mouse-wheelie','memoryleakfix','no-chat-reports','multiconnect', 'sodium','mixintrace', 'mixin-conflict-helper', 'notenoughcrashes');
+my @curseforgeMods = ('litematica', 'minihud', 'tellme', 'tweakeroo','item-scroller', 'worldedit','xaeros-world-map','xaeros-minimap-fair-play-edition', 'yosbr');
 
 for(@modrinthMods){
 	system("packwiz mr install $_ -y");
