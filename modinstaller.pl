10c030ad03215f2b64986c4c52cfa21465ceec2d
Asguho <78410939+Asguho@users.noreply.github.com>
2023-06-13 21:37:49 +0200

    update

--- modinstaller.pl
+++ modinstaller.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 use strict;
-my @modrinthMods = ('exordium', 'fabric-api','c2me-fabric','debugify','ebe','ferritecore-fabric','carpet','ImmediatelyFast','krypton','lazydfu','lithium','starlight','smoothboot-fabric','vmp-fabric', 'modmenu','reeses-sodium-options', 'mouse-wheelie','memoryleakfix','no-chat-reports', 'sodium','mixintrace', 'mixin-conflict-helper', 'notenoughcrashes', 'xaeros-minimap', 'xaeros-world-map', 'yosbr');
+my @modrinthMods = ('exordium', 'fabric-api','c2me-fabric','debugify','ebe','ferritecore-fabric','carpet','ImmediatelyFast','krypton','lazydfu','lithium','starlight','smoothboot-fabric','vmp-fabric', 'modmenu','reeses-sodium-options', 'mouse-wheelie','memoryleakfix','no-chat-reports', 'sodium','mixintrace', 'mixin-conflict-helper', 'notenoughcrashes', 'xaeros-minimap', 'xaeros-world-map', 'yosbr', 'multiconnect , 'sodium-extra' ,'fastload');
 #my @curseforgeMods = ('litematica', 'minihud', 'tellme', 'tweakeroo','item-scroller', 'worldedit');
-#multiconnect , 'sodium-extra' ,'fastload'
+
 for(@modrinthMods){
 	system("packwiz mr install $_ -y");
 }
