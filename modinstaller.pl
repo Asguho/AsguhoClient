f8c3c0d61d361f66603f489fc0eb6a5318a06700
Asguho <78410939+Asguho@users.noreply.github.com>
2023-06-24 18:00:50 +0200

    readded curseforge mods

--- modinstaller.pl
+++ modinstaller.pl
@@ -1,14 +1,14 @@
 #!/usr/bin/perl
 use strict;
 my @modrinthMods = ('exordium', 'fabric-api','c2me-fabric','debugify','ebe','ferritecore-fabric','carpet','ImmediatelyFast','krypton','lazydfu','lithium','starlight','smoothboot-fabric','vmp-fabric', 'modmenu','reeses-sodium-options', 'mouse-wheelie','memoryleakfix','no-chat-reports', 'sodium','mixintrace', 'mixin-conflict-helper', 'notenoughcrashes', 'xaeros-minimap', 'xaeros-world-map', 'yosbr', 'multiconnect', 'sodium-extra', 'fastload');
-#my @curseforgeMods = ('litematica', 'minihud', 'tellme', 'tweakeroo','item-scroller', 'worldedit');
+my @curseforgeMods = ('litematica', 'minihud', 'tellme', 'tweakeroo','item-scroller', 'worldedit');
 
 for(@modrinthMods){
 	system("packwiz mr install $_ -y");
 }
 
-#for(@curseforgeMods){
-#	system("packwiz cf install $_ -y");
-#}
+for(@curseforgeMods){
+	system("packwiz cf install $_ -y");
+}
 
 
