58c6f35998e2d24fddb4ca2b745c4482ec78eb9c
Asguho <78410939+Asguho@users.noreply.github.com>
2024-03-18 12:22:49 +0100

    Update modinstaller.pl

--- modinstaller.pl
+++ modinstaller.pl
@@ -17,7 +17,7 @@ my @performance = (
     "ThreadTweak",
     "Very Many Players"
 );
-my @modrinthMods = ('axiom', 'carpet', 'modmenu','reeses-sodium-options', 'mouse-wheelie','no-chat-reports', 'mixintrace', 'mixin-conflict-helper', 'xaeros-minimap', 'xaeros-world-map', 'yosbr', 'multiconnect', 'sodium-extra', 'why-am-i-on-fire');
+my @modrinthMods = ('axiom', 'carpet', 'modmenu','reeses-sodium-options', 'mouse-wheelie','no-chat-reports', 'mixintrace', 'mixin-conflict-helper', 'xaeros-minimap', 'xaeros-world-map', 'yosbr', 'multiconnect', 'sodium-extra', 'why-am-i-on-fire', "peek");
 my @curseforgeMods = ('litematica', 'minihud', 'tellme', 'tweakeroo','item-scroller');
 ##'notenoughcrashes', 
 
