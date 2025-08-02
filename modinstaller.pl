1b7b66aa493c43d60b5cce7e789e067976962075
Aske Gudmand-Høyer <78410939+Asguho@users.noreply.github.com>
2025-08-02 16:31:30 +0200

    Update modinstaller.pl

--- modinstaller.pl
+++ modinstaller.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl
 use strict;
 my @performance = (
-'BadOptimizations', 'Noisium', 'Entity Culling', 'ScalableLux', 'FerriteCore', 'Concurrent Chunk Management Engine (Fabric)', 'ThreadTweak', 'Lithium', 'Fabric API', 'Very Many Players (Fabric)', 'Sodium', 'No Chat Reports', 'ImmediatelyFast', 'Cloth Config API', 'More Culling', 'ModernFix
+'BadOptimizations', 'Noisium', 'Entity Culling', 'ScalableLux', 'FerriteCore', 'Concurrent Chunk Management Engine (Fabric)', 'ThreadTweak', 'Lithium', 'Fabric API', 'Very Many Players (Fabric)', 'Sodium', 'No Chat Reports', 'ImmediatelyFast', 'Cloth Config API', 'More Culling', 'ModernFix'
 );
 '
-my @modrinthMods = ('axiom', 'carpet', 'modmenu','reeses-sodium-options', 'mouse-wheelie','no-chat-reports', 'mixintrace', 'mixin-conflict-helper', 'xaeros-minimap', 'xaeros-world-map', 'yosbr', 'multiconnect', 'sodium-extra', 'why-am-i-on-fire', "peek", "brb", 'litematica', 'minihud', 'tellme', 'tweakeroo','item-scroller',"distanthorizons");
+my @modrinthMods = ('axiom', 'carpet', 'modmenu','reeses-sodium-options', 'mouse-wheelie','no-chat-reports', 'mixintrace', 'mixin-conflict-helper', 'xaeros-minimap', 'xaeros-world-map', 'yosbr', 'multiconnect', 'sodium-extra', 'why-am-i-on-fire', "peek", "brb", 'litematica', 'minihud', 'tellme', 'tweakeroo','item-scroller',"distanthorizons", 'clientcommands');
 #my @curseforgeMods = ();
 #'notenoughcrashes', 
 
